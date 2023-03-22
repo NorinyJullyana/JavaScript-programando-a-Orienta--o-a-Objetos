df415515706edac12ab491c3d3ee2a2da88b774e
Noriny Jullyana <noriny.rocha@localiza.com>
2023-03-22 14:47:10 -0300

    problema repeticao de variaveis

---
+++ index.js
@@ -0,0 +1,11 @@
+const client1Nome = "Ricardo";
+const client1CPF = "11122233309"
+const client1Agencia = 1001;
+const client1Saldo = 0;
+
+const client2Nome = "Alice";
+const client2CPF = "88822233309"
+const client2Agencia = 1001;
+const client2Saldo = 0;
+
+console.log(client1Nome);
